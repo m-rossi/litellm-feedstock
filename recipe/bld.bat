7ab0475d790621ad5d09ba2c0e57db7b837bdb65
Marco Rossi <developer@marco-rossi.com>
2026-08-18 21:58:48 +0200

    Migrate to v1 recipe, remove run_constrained

--- recipe/bld.bat
+++
@@ -1,4 +0,0 @@
-cd litellm-rust
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cd ..
-%PYTHON -m pip install . -vv --no-deps --no-build-isolation
--- recipe/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd litellm-rust
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cd ..
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
